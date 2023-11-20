e1f49c890a3a3fc428ed22d154daf9dcb1ff0f2b
jean-renoul <jean.renoul@laplateforme.io>
2023-11-20 14:02:32 +0100

    tout finis

---
+++ Jour01/Job10/main.py
@@ -0,0 +1,14 @@
+montant_investissement = 10000
+taux_rendement = 1.15
+
+print (montant_investissement * taux_rendement - montant_investissement)
+
+montant_investissement += 5000
+taux_rendement += 0.02
+
+print (montant_investissement * taux_rendement - montant_investissement)
+
+montant_investissement *= 0.9
+taux_rendement -= 0.01
+
+print (montant_investissement * taux_rendement - montant_investissement)
---
+++ Jour01/pour_aller_plus_loin/main.py
@@ -0,0 +1,9 @@
+phrase = "jean"
+
+x = phrase.count("e")
+
+if x > 0:
+    print ("la phrase",phrase, "contient la lettre e")
+
+if x == 0:
+    print ("la phrase",phrase, "ne contient pas la lettre e")
