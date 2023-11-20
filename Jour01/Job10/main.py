9867b2734e5a412fd54d7eddae39cecb3589e7db
jean-renoul <jean.renoul@laplateforme.io>
2023-11-20 14:15:25 +0100

    tout fini pour de vrai

--- Jour01/Job10/main.py
+++ Jour01/Job10/main.py
@@ -11,4 +11,5 @@ print (montant_investissement * taux_rendement - montant_investissement)
 montant_investissement *= 0.9
 taux_rendement -= 0.01
 
-print (montant_investissement * taux_rendement - montant_investissement)
+print ("le montant de final de l'investissement est de",montant_investissement)
+print ("et le gain final en fonction du taux de rendement est de",montant_investissement * taux_rendement - montant_investissement)
