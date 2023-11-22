b87122b03edf2859246437db2bf9be747bcfd83a
jean-renoul <jean.renoul@laplateforme.io>
2023-11-22 11:20:45 +0100

    job06 fini

---
+++ jour03/job06/main.py
@@ -0,0 +1,8 @@
+def nombre (a):
+    if a < 0:
+        return "Ce nombre est inférieur à 0"
+    elif a > 0:
+        return "Ce nombre est supérieur à 0"
+    
+print (nombre (-5))
+print (nombre (5))
