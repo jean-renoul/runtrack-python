5418f02fba2adbb4c422f7ee296c76df484d463c
jean-renoul <jean.renoul@laplateforme.io>
2023-11-22 12:31:37 +0100

    job09 fait

--- jour03/job09/main.py
+++ jour03/job09/main.py
@@ -7,14 +7,14 @@ def moyenne(a,b,c):
 
 moyenne_etudiant = moyenne(a,b,c)
 
-print (f"la moyenne de l'étdiant est de :{moyenne_etudiant:.1f}")
+print (f"la moyenne de l'étudiant est de :{moyenne_etudiant:.1f}")
 
-if moyenne_etudiant <= 15 and moyenne_etudiant >= 20:
+if 15 <= moyenne_etudiant <=  20:
     print ("Très bon élève")
-elif moyenne_etudiant <= 11 and moyenne_etudiant >= 14:
+elif 11 <= moyenne_etudiant <= 14:
     print ("Bon élève")
-elif moyenne_etudiant <= 8 and moyenne_etudiant >= 10:
+elif 8 <= moyenne_etudiant <= 10:
     print ("Elève moyen")
-elif moyenne_etudiant <= 0 and moyenne_etudiant >= 7:
+elif 0 <= moyenne_etudiant <= 7:
     print ("Elève devant faire des efforts")
     
