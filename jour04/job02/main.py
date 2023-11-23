dc02f5f1ec1b926488772ea2131be962eea70f23
jean-renoul <jean.renoul@laplateforme.io>
2023-11-23 10:38:34 +0100

    job03 et 04

---
+++ jour04/job02/main.py
@@ -0,0 +1,5 @@
+fruits = ["pomme","cerise","orange"]
+secondelement = fruits[1]
+def liste_fruits ():
+    return secondelement
+print (liste_fruits())
---
+++ jour04/job03/main.py
@@ -0,0 +1,5 @@
+fruits = ["pomme","cerise","orange"]
+def liste_fruits ():
+    return fruits.append("Melon")
+liste_fruits()
+print (fruits)
